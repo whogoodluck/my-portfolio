7a729faf0e0aa2889a1b79ba9bd1fd124f6a50ca
sunny <goodluck14115@gmail.com>
2026-02-04 19:56:20 +0530

    Initial commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,92 @@
+import { Github, Instagram, Linkedin, Mail } from "lucide-react";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  const socials = [
+    {
+      name: "linkedin",
+      href: "https://linkedin.com",
+      icon: Linkedin,
+    },
+    {
+      name: "github",
+      href: "https://github.com",
+      icon: Github,
+    },
+    {
+      name: "instagram",
+      href: "https://instagram.com",
+      icon: Instagram,
+    },
+    {
+      name: "email",
+      href: "mailto:contact@shakirfarhan.com",
+      icon: Mail,
+    },
+  ];
+
+  return (
+    <div className="h-screen bg-background text-foreground overflow-hidden relative">
+      <section className="h-full flex flex-col items-center justify-center gap-8 px-4 md:px-16">
+        <div className="md:text-center">
+          <div className="text-xl text-primary font-medium mb-2">
+            <span className="inline-block">Hi there 👋 I am</span>
+          </div>
+
+          <h1 className="md:text-7xl text-5xl font-extrabold mb-6">
+            Sunny kumar
+          </h1>
+
+          <div className="flex items-center md:justify-center gap-2 mb-8">
+            <span className="text-primary font-bold text-lg">{">"}</span>
+            <p className="text-lg text-foreground/60 font-semibold">
+              Full Stack Software Developer
+            </p>
+          </div>
+
+          <p className="md:text-lg text-muted-foreground leading-relaxed mb-8 md:mb-12 max-w-4xl mx-auto">
+            I am highly skilled, confident full-stack developer whose loves
+            building high-quality products from the ground up.
+          </p>
+
+          <div
+            className="flex gap-6 justify-center mb-8 md:mb-12"
+            style={{ animationDelay: "800ms" }}
+          >
+            <Link
+              to="/projects"
+              className="px-4 cursor-pointer py-3  text-sm text-background bg-primary tracking-wide transition-all duration-300 hover:bg-primary/80 rounded-md font-semibold hover:-translate-y-1"
+            >
+              Explore My Work
+            </Link>
+            <Link
+              to="/contact"
+              className="px-4 cursor-pointer py-3 border text-sm bg-muted tracking-wide transition-all duration-300  rounded-md font-semibold hover:-translate-y-1"
+            >
+              Get In Touch
+            </Link>
+          </div>
+
+          <div className="flex gap-6 md:justify-center">
+            {socials.map(({ name, icon, href }) => {
+              const Icon = icon;
+              return (
+                <a
+                  key={name}
+                  href={href}
+                  target={name !== "email" ? "_blank" : undefined}
+                  rel={name !== "email" ? "noopener noreferrer" : undefined}
+                  className="size-10 flex items-center justify-center rounded-full bg-muted text-primary/80 transition-all duration-300  hover:-translate-y-1 hover:rotate-[5deg] hover:bg-primary/10 hover:text-primary"
+                >
+                  <Icon className="size-5" />
+                </a>
+              );
+            })}
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Home;
